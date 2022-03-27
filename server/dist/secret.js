5ce71233a4cf6b1cf9d887d24569b553db9aa77b
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2022-03-27 00:21:33 -0300

    [DEL] server/node_modules and server/dist

--- server/dist/secret.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// TODO: Change the secret
-const SECRET = 'SUPER SECRET PHRASE';
-exports.default = SECRET;
-//# sourceMappingURL=secret.js.map
